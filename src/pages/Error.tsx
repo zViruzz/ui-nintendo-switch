987eba45a837b0cdcab08569cb200e7414cb813d
viruz <nahuelsinka@gmail.com>
2023-12-05 19:39:25 -0300

    QUIERO DESCANSAR

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,11 @@
 import { useRouteError } from 'react-router-dom'
 
 function Error () {
-  const { message, statusText } = useRouteError()
+  const { message, statusText } = useRouteError() as { message: string, statusText: string }
   return (
     <div>
       <h1>Error</h1>
-      <p>{message || statusText}</p>
+      <p>{message ?? statusText}</p>
     </div>
   )
 }
