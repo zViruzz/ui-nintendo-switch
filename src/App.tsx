570b35d04d222dd73db4b306c8f1d2da883ac020
Viruz <98500701+V-Viruzz@users.noreply.github.com>
2024-06-22 02:10:36 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import './global.css'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import { ControllerProvider } from './context/controller'
+import { useTranslation } from 'react-i18next'
 
 function App () {
+  const { t } = useTranslation()
+
   return (
     <>
       <ControllerProvider>
         <main>
+          <div className='absolute w-screen h-screen bg-[#0000008a]  z-40 grid place-content-center'>
+            <div className='bg-neutral-600 w-96 h-60 rounded-2xl'>
+              <h2>Nintendo Switch UI</h2>
+              <p>{t('start.message.description')}</p>
+            </div>
+          </div>
           <Outlet />
         </main>
         <Footer />
