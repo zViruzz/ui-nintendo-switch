0f0c82ffa319fa148fa1c30a80da4f8a449898e7
Viruzz <nahuelsinka@gmail.com>
2024-07-01 00:00:27 -0300

    refactoring

--- src/static/game.ts
+++ src/static/game.ts
@@ -1,4 +1,8 @@
 import ytLogo from '../assets/youtube-logo.png'
+import AnimalCrossingUrl from '../assets/games/animal-crossing-new-horizons.jpg'
+import ZeldaBotwUrl from '../assets/games/zelda-botw.webp'
+import PokemonShieldUrl from '../assets/games/pokemon-shield.webp'
+import SumerMarioMakerUrl from '../assets/games/super-mario-maker.webp'
 
 export const game = [
   {
@@ -9,23 +13,23 @@ export const game = [
   {
     id: '2',
     name: 'Pokemon Shield',
-    urlImage: 'https://cdn.dekudeals.com/images/c5b6b05b17a96b67899c7e1fd718974fc7424038/w500.jpg'
+    urlImage: PokemonShieldUrl
   },
   {
     id: '3',
     name: 'The Legend of Zelda: BTW',
-    urlImage: 'https://th.bing.com/th/id/R.eac2e92b694df845e0f655d228329802?rik=5gyHI2QiGDeyOg&pid=ImgRaw&r=0'
+    urlImage: ZeldaBotwUrl
   },
 
   {
     id: '4',
     name: 'Super Mario Maker 2',
-    urlImage: 'https://th.bing.com/th/id/R.268123175c53d3ffb984dc21541eb80d?rik=J2KRXLIs3gUu6A&pid=ImgRaw&r=0'
+    urlImage: SumerMarioMakerUrl
   },
   {
     id: '5',
     name: 'Animal Crossing',
-    urlImage: 'https://www.mobygames.com/images/covers/l/632236-animal-crossing-new-horizons-nintendo-switch-front-cover.jpg'
+    urlImage: AnimalCrossingUrl
   }
 
 ]
