d166f30894a97aac2ef00c6d64f7ab1bcc77f19f
Viruzz <nahuelsinka@gmail.com>
2024-07-20 01:39:20 -0300

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Header ({ children, className = '' }: Props) {
   return (
-    <header className={`${className} col-span-2 px-12 flex justify-between flex-col`}>
+    <header className={`${className} col-span-2 px-[2.6%] flex justify-between flex-col`}>
       <div className='flex items-center text-[3rem] w-full h-full px-[3rem] gap-4 border-b-2 '>
         {children}
       </div>
--- src/pages/Controllers/ControllersRoot/ControllersRoot.tsx
+++ src/pages/Controllers/ControllersRoot/ControllersRoot.tsx
@@ -6,24 +6,24 @@ import BatteryIcon from '../../../components/icons/BatteryIcon'
 
 export function ControllersRoot () {
   return (
-    <div className='w-full h-full grid grid-rows-[1fr_7.5fr] grid-cols-[1fr_42rem]'>
+    <div className='w-full h-full grid grid-rows-[1fr_7.5fr] grid-cols-[3fr_1.8fr]'>
       <Header>
         <ControllerIcon width={83} height={83} />
         <h2>Controllers</h2>
       </Header>
 
-      <div className='pl-12 grid grid-rows-[1fr_9.7rem] pr-11'>
+      <div className='pl-[4.5%] grid grid-rows-[4.3fr_1fr] pr-[4.5%]'>
         <div className='flex flex-col justify-center items-center'>
           <div className='text-4xl flex-1 flex flex-col-reverse text-description'>
-            <p className='py-5'>Console</p>
+            <p className='leading-[1.9]'>Console</p>
           </div>
           <img
             className='w-[38%]'
             src={nintendoSwitchIcon}
             alt="Nintendo Switch Icon"
           />
-          <div className='flex-1 text-[52px]'>
-            <div className='pt-3 flex gap-16'>
+          <div className='flex-1 text-[52px] w-full'>
+            <div className='pt-3 flex gap-[7%] w-full justify-center'>
               <BatteryIcon />
               <BatteryIcon />
               <BatteryIcon />
@@ -35,7 +35,7 @@ export function ControllersRoot () {
         </div>
       </div>
 
-      <nav className='min-h-full pr-12 bg-gradient-to-t from-transparent from-5% via-[#4e4e4e] via-50% to-transparent to-100% text-secodary text-[2.6rem]'>
+      <nav className='min-h-full pr-[2.6%] bg-gradient-to-t from-transparent from-5% via-[#4e4e4e] via-50% to-transparent to-100% text-secodary text-[2.6rem]'>
         <ul className='h-full w-full pl-12 pt-10 flex flex-col justify-evenly hover:[&>li>a]:bg-[#252d328a] focus-visible:[&>li>a]:bg-[#252d328a] [&>li>a]:outline-wiggle-focus [&>li]:w-full [&>li]:h-[7.4rem] [&>li>a]:w-full [&>li>a]:h-full [&>li>a]:flex [&>li>a]:items-center [&>li>a]:px-5'>
           <li>
             <Link to='/change-grip-or-order'>
