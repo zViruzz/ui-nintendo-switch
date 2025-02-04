5ebd1013f7c9483cdf52a2003b980ff8a20a0547
zViruzz <nahuelsinka@gmail.com>
2025-02-03 23:46:48 -0300

    fix: :bug: types

--- src/context/controller.tsx
+++ src/context/controller.tsx
@@ -1,101 +1,101 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, type ReactNode, useContext, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { type To, useNavigate } from 'react-router-dom'
 
 export interface ControllerButtonParams {
-	text: string
-	route?: To | number
-	action?: () => void
+  text: string
+  route?: To | number
+  action?: () => void
 }
 
 interface Props {
-	children: JSX.Element | JSX.Element[]
+  children: ReactNode | ReactNode[]
 }
 
 interface ContextProps {
-	controllerButtonA: (params: ControllerButtonParams) => void
-	controllerButtonB: (params: ControllerButtonParams) => void
-	buttonA: {
-		text: string
-		onClick: () => void
-	}
-	buttonB: {
-		text: string
-		onClick: () => void
-	}
+  controllerButtonA: (params: ControllerButtonParams) => void
+  controllerButtonB: (params: ControllerButtonParams) => void
+  buttonA: {
+    text: string
+    onClick: () => void
+  }
+  buttonB: {
+    text: string
+    onClick: () => void
+  }
 }
 
 export const ControllerContext = createContext<ContextProps>({
-	controllerButtonA: () => {},
-	controllerButtonB: () => {},
-	buttonA: {
-		text: '',
-		onClick: () => {},
-	},
-	buttonB: {
-		text: '',
-		onClick: () => {},
-	},
+  controllerButtonA: () => { },
+  controllerButtonB: () => { },
+  buttonA: {
+    text: '',
+    onClick: () => { },
+  },
+  buttonB: {
+    text: '',
+    onClick: () => { },
+  },
 })
 
 export const ControllerProvider: React.FC<Props> = ({ children }) => {
-	const navigate = useNavigate()
-	const { t } = useTranslation()
+  const navigate = useNavigate()
+  const { t } = useTranslation()
 
-	const [buttonA, setButtonA] = useState({
-		text: '',
-		onClick: () => {},
-	})
+  const [buttonA, setButtonA] = useState({
+    text: '',
+    onClick: () => { },
+  })
 
-	const [buttonB, setButtonB] = useState({
-		text: '',
-		onClick: () => {},
-	})
+  const [buttonB, setButtonB] = useState({
+    text: '',
+    onClick: () => { },
+  })
 
-	const controllerButtonA = ({ text, action, route }: ControllerButtonParams) => {
-		setButtonA({
-			text: t(text),
-			onClick: () => {
-				if (route !== undefined) {
-					if (typeof route === 'string') {
-						navigate(route) // Navegar a una ruta específica
-					} else if (typeof route === 'number') {
-						navigate(route) // Navegar en el historial
-					}
-				}
-				if (action !== undefined) action()
-			},
-		})
-	}
+  const controllerButtonA = ({ text, action, route }: ControllerButtonParams) => {
+    setButtonA({
+      text: t(text),
+      onClick: () => {
+        if (route !== undefined) {
+          if (typeof route === 'string') {
+            navigate(route) // Navegar a una ruta específica
+          } else if (typeof route === 'number') {
+            navigate(route) // Navegar en el historial
+          }
+        }
+        if (action !== undefined) action()
+      },
+    })
+  }
 
-	const controllerButtonB = ({ text, action, route }: ControllerButtonParams) => {
-		setButtonB({
-			text: t(text),
-			onClick: () => {
-				if (route !== undefined) {
-					if (typeof route === 'string') {
-						navigate(route) // Navegar a una ruta específica
-					} else if (typeof route === 'number') {
-						navigate(route) // Navegar en el historial
-					}
-				}
-				if (action !== undefined) action()
-			},
-		})
-	}
+  const controllerButtonB = ({ text, action, route }: ControllerButtonParams) => {
+    setButtonB({
+      text: t(text),
+      onClick: () => {
+        if (route !== undefined) {
+          if (typeof route === 'string') {
+            navigate(route) // Navegar a una ruta específica
+          } else if (typeof route === 'number') {
+            navigate(route) // Navegar en el historial
+          }
+        }
+        if (action !== undefined) action()
+      },
+    })
+  }
 
-	return (
-		<ControllerContext.Provider
-			value={{
-				controllerButtonA,
-				controllerButtonB,
-				buttonA,
-				buttonB,
-			}}
-		>
-			{children}
-		</ControllerContext.Provider>
-	)
+  return (
+    <ControllerContext.Provider
+      value={{
+        controllerButtonA,
+        controllerButtonB,
+        buttonA,
+        buttonB,
+      }}
+    >
+      {children}
+    </ControllerContext.Provider>
+  )
 }
 
 export const useControllerContext = (): ContextProps => useContext(ControllerContext)
--- src/context/optionsMenu.tsx
+++ src/context/optionsMenu.tsx
@@ -1,107 +1,107 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, type ReactNode, useContext, useState } from 'react'
 import { useControllerContext } from './controller'
 
 type Option = {
-	label: string
-	isOn?: boolean
+  label: string
+  isOn?: boolean
 }
 
 interface ListOptions {
-	isHidden: boolean
-	options: Option[]
+  isHidden: boolean
+  options: Option[]
 }
 
 interface Props {
-	children: JSX.Element | JSX.Element[]
+  children: ReactNode | ReactNode[]
 }
 
 interface ContextProps {
-	listOptions: ListOptions
-	setListOptions: (setting: ListOptions) => void
-	onToggleHidden: (bol: boolean) => void
-	configureListOptions: (setting: ListOptions) => void
-	activeOption: (index: number) => void
+  listOptions: ListOptions
+  setListOptions: (setting: ListOptions) => void
+  onToggleHidden: (bol: boolean) => void
+  configureListOptions: (setting: ListOptions) => void
+  activeOption: (index: number) => void
 }
 
 export const OptionsMenuContext = createContext<ContextProps>({
-	listOptions: {
-		isHidden: true,
-		options: [
-			{
-				label: 'Ok',
-				isOn: false,
-			},
-		],
-	},
-	activeOption: () => {},
-	setListOptions: () => {},
-	onToggleHidden: () => {},
-	configureListOptions: () => {},
+  listOptions: {
+    isHidden: true,
+    options: [
+      {
+        label: 'Ok',
+        isOn: false,
+      },
+    ],
+  },
+  activeOption: () => { },
+  setListOptions: () => { },
+  onToggleHidden: () => { },
+  configureListOptions: () => { },
 })
 
 export const OptionsMenuProvider: React.FC<Props> = ({ children }) => {
-	const [listOptions, setListOptions] = useState<ListOptions>({
-		isHidden: true,
-		options: [
-			{
-				label: 'Close',
-				isOn: false,
-			},
-		],
-	})
+  const [listOptions, setListOptions] = useState<ListOptions>({
+    isHidden: true,
+    options: [
+      {
+        label: 'Close',
+        isOn: false,
+      },
+    ],
+  })
 
-	const { controllerButtonB } = useControllerContext()
+  const { controllerButtonB } = useControllerContext()
 
-	const configureOptionsList = (listOptions: ListOptions) => {
-		setListOptions(listOptions)
+  const configureOptionsList = (listOptions: ListOptions) => {
+    setListOptions(listOptions)
 
-		controllerButtonB({
-			text: 'controller.buttonB.back',
-			action: () => {
-				console.log('optionmenu B')
-				onToggleHidden(true)
-			},
-		})
+    controllerButtonB({
+      text: 'controller.buttonB.back',
+      action: () => {
+        console.log('optionmenu B')
+        onToggleHidden(true)
+      },
+    })
 
-		setTimeout(() => {
-			const section = document.querySelector('#card-message')
-			if (section instanceof HTMLDivElement) section.focus()
-		}, 200)
-	}
+    setTimeout(() => {
+      const section = document.querySelector('#card-message')
+      if (section instanceof HTMLDivElement) section.focus()
+    }, 200)
+  }
 
-	const onToggleHidden = (value: boolean) => {
-		setListOptions((prev) => ({
-			...prev,
-			isHidden: value,
-		}))
-	}
+  const onToggleHidden = (value: boolean) => {
+    setListOptions((prev) => ({
+      ...prev,
+      isHidden: value,
+    }))
+  }
 
-	const activeOption = (index: number) => {
-		setListOptions((prev) => ({
-			...prev,
-			options: prev.options.map((option, i) => {
-				if (i === index) {
-					return { ...option, isOn: true }
-				}
+  const activeOption = (index: number) => {
+    setListOptions((prev) => ({
+      ...prev,
+      options: prev.options.map((option, i) => {
+        if (i === index) {
+          return { ...option, isOn: true }
+        }
 
-				return { ...option, isOn: false }
-			}),
-		}))
-	}
+        return { ...option, isOn: false }
+      }),
+    }))
+  }
 
-	return (
-		<OptionsMenuContext.Provider
-			value={{
-				activeOption,
-				listOptions,
-				setListOptions,
-				onToggleHidden,
-				configureListOptions: configureOptionsList,
-			}}
-		>
-			{children}
-		</OptionsMenuContext.Provider>
-	)
+  return (
+    <OptionsMenuContext.Provider
+      value={{
+        activeOption,
+        listOptions,
+        setListOptions,
+        onToggleHidden,
+        configureListOptions: configureOptionsList,
+      }}
+    >
+      {children}
+    </OptionsMenuContext.Provider>
+  )
 }
 
 export const useOptionsMenuContext = (): ContextProps => useContext(OptionsMenuContext)
