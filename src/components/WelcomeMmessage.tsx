c0078f93e5b2ff677d00e0ea02f61438a6ea639e
zViruzz <nahuelsinka@gmail.com>
2025-02-22 01:53:12 -0300

    style: :lipstick: welcomCard update

--- src/components/WelcomeMmessage.tsx
+++ src/components/WelcomeMmessage.tsx
@@ -22,24 +22,26 @@ export default function WelcomeMmessage() {
 		<div
 			className={cn(
 				'absolute w-screen h-screen bg-[#0c1720b1] z-40 flex justify-center items-center transition-all',
-				hiddenMessage ? 'invisible opacity-0' : 'visible',
+				hiddenMessage ? '' : 'visible',
 			)}
 		>
 			<div className='bg-gray-message-light dark:bg-gray-message w-[72rem] h-[35rem] rounded-lg flex flex-col relative'>
-				<NintendoSwitchLogoIcon className='absolute h-[8rem] w-auto top-7 right-10 opacity-30' />
+				<NintendoSwitchLogoIcon className='absolute h-[8rem] w-auto top-7 right-10 opacity-30 text-neutral-500 dark:text-white' />
 				<div className='w-full h-full text-4xl px-28 pt-20'>
 					<p className='font-semibold'>{t('welcom.message.welcom')}</p>
 					<br />
 					<p>{t('welcom.message.suggestion')}</p>
-					<p className='text-description text-[35px] mt-6'>{t('welcom.message.note')}</p>
+					<p className='dark:text-description text-description-light text-[35px] mt-6'>
+						{t('welcom.message.note')}
+					</p>
 				</div>
-				<div className='w-full h-36'>
+				<div className='w-full h-36 border-t-2 dark:border-neutral-500 border-neutral-300'>
 					<button
 						onClick={handleClick}
-						className='bg-blue-hight-focus outline-wiggle-focus focus:bg-opacity-40 hover:bg-opacity-40 bg-transparent border-0 border-t-2 border-t-white border-opacity-20 text-secodary w-full h-full rounded-xs text-4xl active:bg-secodary/[.06] active:outline-0 transition-all duration-75'
+						className='bg-blue-hight-focus outline-wiggle-focus focus:bg-opacity-40 hover:bg-opacity-40 bg-transparent border-0 text-secodary-light dark:text-secodary w-full h-full rounded-xs text-4xl active:bg-secodary/[.06] active:outline-0 transition-all duration-75'
 						type='button'
 					>
-						OK
+						Ok
 					</button>
 				</div>
 			</div>
