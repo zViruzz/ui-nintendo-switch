1a2df13a0f669c817a7a4a0885f9ff5e68f9f33c
zViruzz <nahuelsinka@gmail.com>
2025-03-07 17:38:38 -0300

    feat: :sparkles: animation of menu buttons

--- src/components/MenuHome.tsx
+++ src/components/MenuHome.tsx
@@ -1,8 +1,8 @@
-import type { ReactNode } from 'react'
+import { useState, type ReactNode } from 'react'
 // import huhSound from '../assets/huh.mp3'
 import { motion } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useControllerContext } from '../context/controller'
 import ControllerIcon from '../icons/ControllerIcon'
 import GalleryIcon from '../icons/GalleryIcon'
@@ -51,6 +51,8 @@ function ButtonMenu({
 }: { children: ReactNode; router: string; text: string }) {
 	// const sound = new window.Audio(huhSound)
 	const { controllerButtonA } = useControllerContext()
+	const [isAnimating, setIsAnimating] = useState(false)
+	const navigate = useNavigate()
 
 	const handleFocus = () => {
 		controllerButtonA({
@@ -61,6 +63,13 @@ function ButtonMenu({
 			},
 		})
 	}
+	const handleClick = () => {
+		setIsAnimating(true)
+
+		setTimeout(() => {
+			navigate(router)
+		}, 300)
+	}
 
 	return (
 		<motion.li
@@ -68,22 +77,39 @@ function ButtonMenu({
 				// sound.play()
 				console.log('huh')
 			}}
-			className='ButtonMenu group text-font-highlight-light dark:text-font-highlight text-center text-[2.5rem] relative cursor-pointer rounded-full'
+			className='ButtonMenu group text-font-highlight-light dark:text-font-highlight text-center text-[2.5rem] relative rounded-full '
 		>
-			<Link
-				className='grid place-content-center outline-8 outline-none group-hover:outline-solid group-focus-within:outline-solid group-hover:animate-wiggle group-focus-within:animate-wiggle bg-[#fefffe] dark:bg-[#505050] text-[#717a7a] dark:text-[#d9d9d9] w-full h-full rounded-full shadow-Nbutton transition-all'
+			<button
+				type='button'
+				className='flex outline-8 outline-none group-hover:outline-solid group-focus-within:outline-solid group-hover:animate-wiggle group-focus-within:animate-wiggle bg-[#fefffe] dark:bg-[#505050] text-[#717a7a] dark:text-[#d9d9d9] w-full h-full rounded-full shadow-Nbutton transition-all cursor-pointer'
 				onFocus={handleFocus}
-				to={router}
+				onClick={handleClick}
 			>
 				<motion.span
-					className='h-full w-full rounded-full'
+					className='h-full w-full flex justify-center items-center rounded-full'
 					tabIndex={-1}
-					whileTap={{ rotate: 20 }}
-					transition={{ duration: 0.05 }}
+					animate={
+						isAnimating
+							? {
+									outline: [
+										'solid 1px rgb(255,255,255,0.7)',
+										'solid 22px rgb(255,255,255,0)',
+									],
+									backgroundColor: 'rgba(255, 255, 255, 0.15)',
+									rotate: [20, -20, 0],
+									x: [0, -4, 4, -4, 4, 0],
+									y: [0, -2, 2, -2, 2, 0],
+								}
+							: {}
+					}
+					transition={{
+						duration: 0.2,
+						when: 'beforeChildren',
+					}}
 				>
 					{children}
 				</motion.span>
-			</Link>
+			</button>
 
 			{/* <div className='bg-[#fefffe] dark:bg-[#505050] text-[#717a7a] dark:text-[#d9d9d9] w-full h-full rounded-full grid place-content-center group-hover:outline-8 group-focus-within:outline-8 group-hover:animate-wiggle group-focus-within:animate-wiggle shadow-Nbutton transition-all duration-100'> */}
 			{/* 	<Link */}
