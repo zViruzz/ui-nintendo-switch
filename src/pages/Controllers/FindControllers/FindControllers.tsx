f43603ef9e0628a36d00d2110ed395d0ae255f30
zViruzz <nahuelsinka@gmail.com>
2025-03-09 18:27:18 -0300

    feat: :sparkles: interactive animation with controls in the find-controllers route

--- src/pages/Controllers/FindControllers/FindControllers.tsx
+++ src/pages/Controllers/FindControllers/FindControllers.tsx
@@ -2,6 +2,81 @@ import { Link } from 'react-router-dom'
 import controllersLeftUrl from '../../../assets/images/controllers-left.webp'
 import controllersRightUrl from '../../../assets/images/controllers-right.webp'
 import OpacityPageTransition from '../../../transitions/OpacityPageTransition'
+import QuestionIcon from '../../../icons/QuestionIcon'
+import { useAnimate, type ValueAnimationTransition } from 'framer-motion'
+import { type KeyboardEvent, useState } from 'react'
+import cn from '../../../utils/cn'
+
+type VibrationAnimationConfig = {
+	keyframes: { x: number[] | number }
+	options: ValueAnimationTransition
+}
+
+type ControllerButtonProps = {
+	imageSrc: string
+	imageAlt: string
+}
+
+function ControllerButton({ imageSrc, imageAlt }: ControllerButtonProps) {
+	const [isActive, setIsActive] = useState(false)
+	const [controllerRef, animate] = useAnimate()
+
+	const vibrationAnimation: VibrationAnimationConfig = {
+		keyframes: { x: [-7, 7, 0] },
+		options: {
+			duration: 0.3,
+			// biome-ignore lint/style/useNumberNamespace: <explanation>
+			repeat: Infinity,
+			repeatType: 'loop',
+		},
+	}
+
+	const stopAnimation: VibrationAnimationConfig = {
+		keyframes: { x: 0 },
+		options: { duration: 0.3, repeat: 0 },
+	}
+
+	const handleStart = () => {
+		setIsActive(true)
+		animate(
+			controllerRef.current,
+			vibrationAnimation.keyframes,
+			vibrationAnimation.options,
+		)
+	}
+
+	const handleStop = () => {
+		setIsActive(false)
+		animate(controllerRef.current, stopAnimation.keyframes, stopAnimation.options)
+	}
+
+	const handleKeyDown = (e: KeyboardEvent<HTMLButtonElement>) => {
+		if (e.key === 'Enter' || e.key === ' ') {
+			handleStart()
+		}
+	}
+
+	return (
+		<button
+			type='button'
+			className={cn(
+				'w-full h-full flex flex-col justify-evenly items-center dark:bg-[#767676] bg-background-highlight-light outline-wiggle-focus active:bg-teal-200/30',
+				{ 'dark:bg-teal-200/30': isActive },
+			)}
+			onKeyDown={handleKeyDown}
+			onKeyUp={handleStop}
+			onMouseDown={handleStart}
+			onMouseUp={handleStop}
+			onMouseLeave={handleStop}
+			tabIndex={0}
+		>
+			<p className='text-[27px]'>Console</p>
+			<img ref={controllerRef} className='w-[22%]' src={imageSrc} alt={imageAlt} />
+			<div />
+			<div />
+		</button>
+	)
+}
 
 export default function FindControllers() {
 	return (
@@ -14,30 +89,12 @@ export default function FindControllers() {
 					Press ; or r on a searby controller to make the selected controller vibrate
 				</p>
 			</div>
-			<div className='px-[6.2%] py-[1.7%] gap-4 grid grid-cols-res-controllers xl:grid-rows-3 2xl:grid-rows-2 [&>div]:bg-primary-light [&>div]:shadow-button'>
+			<div className='px-[6.2%] py-[1.7%] gap-4 grid grid-cols-res-controllers xl:grid-rows-3 2xl:grid-rows-2 [&>div]:bg-neutral-600 [&>div]:shadow-button'>
 				<div>
-					<div className='w-full h-full flex flex-col justify-evenly items-center dark:bg-[#767676] background-highlight-light'>
-						<p className='text-[27px]'>Console</p>
-						<img
-							className='w-[22%]'
-							src={controllersLeftUrl}
-							alt='controllers left url'
-						/>
-						<div />
-						<div />
-					</div>
+					<ControllerButton imageSrc={controllersLeftUrl} imageAlt='Left controller' />
 				</div>
 				<div>
-					<div className='w-full h-full flex flex-col justify-evenly items-center dark:bg-[#767676] background-highlight-light'>
-						<p className='text-[27px]'>Console</p>
-						<img
-							className='w-[22%]'
-							src={controllersRightUrl}
-							alt='controllers left url'
-						/>
-						<div />
-						<div />
-					</div>
+					<ControllerButton imageSrc={controllersRightUrl} imageAlt='Right controller' />
 				</div>
 				<div />
 				<div />
@@ -48,7 +105,7 @@ export default function FindControllers() {
 				<div />
 				<div />
 			</div>
-			<div className='text-2xl md:text-3xl lg:text-4xl flex gap-[5.5%] items-end px-[5%]'>
+			<div className='text-2xl md:text-3xl lg:text-[2.40rem] flex gap-[5.5%] items-end px-[5%]'>
 				<Link
 					className='border-[3px] dark:border-white border-neutral-600 w-full h-[6.6rem] bg-transparent flex justify-center items-center rounded-lg outline-offset-[-3px] outline-wiggle-focus background-highlight'
 					to='/controllers'
@@ -56,10 +113,11 @@ export default function FindControllers() {
 					Back
 				</Link>
 				<button
-					className='border-[3px] dark:border-white border-neutral-600 w-full h-[6.6rem] bg-transparent rounded-lg outline-offset-[-3px] outline-wiggle-focus background-highlight'
+					className='flex justify-center items-center gap-3 border-[3px] dark:border-white border-neutral-600 w-full h-[6.6rem] bg-transparent rounded-lg outline-offset-[-3px] outline-wiggle-focus background-highlight'
 					type='button'
 				>
-					of controllers can t be found
+					<QuestionIcon width={43} height={43} />
+					if controllers can t be found
 				</button>
 			</div>
 		</OpacityPageTransition>
