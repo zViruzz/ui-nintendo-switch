c7c610646abdc36e66f527a70b4c3baf5c2c06de
zviruz <nahuelsinka@gmail.com>
2025-04-30 11:03:26 -0300

    fix: :bug: Safely invoke onClick handlers in CardMessage component

--- src/components/CardMessage/CardMessage.tsx
+++ src/components/CardMessage/CardMessage.tsx
@@ -40,7 +40,7 @@ export default function CardMessage() {
 									if (item.isCloseButton) {
 										onToggleHidden(true)
 									}
-									item.onClick()
+									item.onClick?.()
 								}}
 							>
 								{item.label}
@@ -56,7 +56,7 @@ export default function CardMessage() {
 									if (setting.buttons.isCloseButton) {
 										onToggleHidden(true)
 									}
-									setting.buttons.onClick()
+									setting.buttons.onClick?.()
 								}
 							}}
 						>
