6d4df31dc559b1d6c3a3f52bc00856ecb69782bd
zviruz <nahuelsinka@gmail.com>
2025-07-11 18:52:25 -0300

    refactor: :sparkles: Replace main tags with EshopOptionsContainer component

--- src/pages/Eshop/Home/options/Featured.tsx
+++ src/pages/Eshop/Home/options/Featured.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router'
 import imageUrlNintendoSwitch2 from '../../../../assets/NSwitch2_HWKeyvisual.jpg'
 import imageUrlLuigisMansion from '../../../../assets/luigis-mansion.webp'
 import imageUrlMarioKart from '../../../../assets/mario-kart-8-deluxe.webp'
@@ -6,18 +5,14 @@ import imageUrlSuperMarioBros from '../../../../assets/super-mario-bros.webp'
 import imageUrlSuperMario from '../../../../assets/super-mario-odyssey.webp'
 import imageUrlZelda from '../../../../assets/zelda.webp'
 import { CardGameFeatured } from '../../../../components/Eshop/CardGameFeatured'
-import CharacterProfile from '../../../../components/CharacterProfile'
-import ArrowThisRightIcon from '../../../../icons/ArrowThinRight'
 import ListPageTransition from '../../../../transitions/ListPageTransition'
 import { HeaderTitle } from '../../../../components/Eshop/HeaderTitle'
+import { EshopOptionsContainer } from '../../../../ui/EshopOptionsContainer'
 
 export function Featured() {
 	return (
 		<ListPageTransition>
-			<main
-				tabIndex={-1}
-				className='w-full h-full px-[6.5%] py-18 pb-40 overflow-auto focus-main'
-			>
+			<EshopOptionsContainer>
 				<HeaderTitle title='Featured' />
 				<div className='w-full mt-9 grid grid-cols-2 gap-3 grid-rows-3'>
 					<CardGameFeatured
@@ -36,7 +31,7 @@ export function Featured() {
 					/>
 					<CardGameFeatured title={`Luigi's Mansion`} urlImage={imageUrlLuigisMansion} />
 				</div>
-			</main>
+			</EshopOptionsContainer>
 		</ListPageTransition>
 	)
 }
--- src/pages/Eshop/Home/options/RecentReleases.tsx
+++ src/pages/Eshop/Home/options/RecentReleases.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router'
-import CharacterProfile from '../../../../components/CharacterProfile'
 import ListPageTransition from '../../../../transitions/ListPageTransition'
 import { CardGameRecent } from '../../../../components/Eshop/CardGameRecent'
 
@@ -9,11 +7,12 @@ import imageUrlSuperMarioBros from '../../../../assets/super-mario-bros.webp'
 import imageUrlSuperMario from '../../../../assets/super-mario-odyssey.webp'
 import imageUrlZelda from '../../../../assets/zelda.webp'
 import { HeaderTitle } from '../../../../components/Eshop/HeaderTitle'
+import { EshopOptionsContainer } from '../../../../ui/EshopOptionsContainer'
 
 export function RecentRealeases() {
 	return (
 		<ListPageTransition>
-			<main className='w-full h-full px-[6.5%] py-18 pb-40 overflow-auto'>
+			<EshopOptionsContainer>
 				<HeaderTitle title='Recent Realeases' />
 				<div className='w-full mt-9 grid grid-cols-3 gap-3 grid-rows-3'>
 					<CardGameRecent price='69.99' urlImage={imageUrlSuperMario} />
@@ -22,7 +21,7 @@ export function RecentRealeases() {
 					<CardGameRecent price='69.99' urlImage={imageUrlZelda} />
 					<CardGameRecent price='49.99' urlImage={imageUrlLuigisMansion} />
 				</div>
-			</main>
+			</EshopOptionsContainer>
 		</ListPageTransition>
 	)
 }
